5898d0d4585dfbd161c9f824cb78a43aec0bbafc
srikanth <ksrao2k17@gmail.com>
2024-01-05 16:24:17 +0530

    Revert "first commit"
    This reverts commit 203ab49b1042cb31bfd4a8ab9db2f80c4dfd3f7d.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 
-interface SideNavToggle{
-  screenWidth: number;
-  collapsed: boolean;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,14 +7,4 @@ interface SideNavToggle{
 })
 export class AppComponent {
   title = 'SideNav';
-
-  isSideNavCollapsed = false;
-  screenWidth= 0;
-
-  onToggleSideNav(data: SideNavToggle): void{
-    this.screenWidth = data.screenWidth;
-    this.isSideNavCollapsed = data.collapsed;
-
-  }
-
 }
